6c50a954bcf166e4dd0ea35915c742556cae2b31
AlexanderAlbaniel77 <alexanderalbaniel77@gmail.com>
2025-11-18 01:47:55 +0800

    Complete CRUD functionality - Create, Read, Update, Delete

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    
-    
-    path('', views.HomePageView.as_view(), name='home'),
-    
+    path('', views.HomePageView.as_view(), name='home'),  
     path('about/', views.AboutPageView.as_view(), name='about'),
+    path('blog/', views.BlogListView.as_view(), name='blog_list'),
+    path('blog/<int:pk>/', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/create/', views.BlogCreateView.as_view(), name='blog_create'),
+    path('blog/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='blog_update'),
+    path('blog/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='blog_delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Post
 
 class HomePageView(TemplateView):
     template_name = "app/home.html"
 
-
 class AboutPageView(TemplateView):
     template_name = "app/About.html"
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "app/blog_list.html"
+    context_object_name = "posts"
+
+class BlogDetailView(DetailView):
+    model = Post
+    context_object_name = "post"
+    template_name = "app/blog_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "app/blog_create.html"
+    fields = ['title', 'body', 'author']
+    success_url = '/blog/'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "app/blog_update.html"
+    fields = ['title', 'body']
+    success_url = '/blog/'
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "app/blog_delete.html"
+    success_url = '/blog/'
